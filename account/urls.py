8183069b5adc09ac4eb1d4adfbaa73c4ce37d591
Ruloong <me@rulong.org>
2013-08-22 16:44:34 +0800

    add query detail

--- account/urls.py
+++ account/urls.py
@@ -2,9 +2,11 @@ __author__ = 'rulongwang'
 from django.conf.urls import patterns, include, url
 from account.views import auth_home, register
 from account.views import login_view,logout_view, register
+from query.views import getDetail
 urlpatterns = patterns('',
-    (r'login/$', login_view),
-    (r'logout/$', logout_view),
+    (r'^login/$', login_view),
+    (r'^logout/$', logout_view),
     (r'^register/$', register),
-    (r'home/$',auth_home),
+    (r'^home/$',auth_home),
+    (r'^getDetail/$', getDetail),
 )
--- query/views.py
+++ query/views.py
@@ -1,6 +1,11 @@
 # Create your views here.
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render_to_response,render
+from django.shortcuts import render_to_response,HttpResponse,  RequestContext, HttpResponseRedirect, get_object_or_404
+from django.views.decorators.csrf import csrf_protect
+from django.core.urlresolvers import reverse
+import urllib
+import json
 # from PIL import Image
 
 #register app entry
@@ -14,3 +19,23 @@ def hello(request):
 
 def getIcon(request):
     pass
+
+
+
+
+def getDetail(request):
+    """
+
+    :param request:
+    :return:
+    """
+    search_url = 'http://itunes.apple.com/lookup?id=639384326'
+    raw = urllib.urlopen(search_url)
+    js = raw.read()
+    js_object = json.loads(js)
+    results = js_object["results"][0]
+    icon = results['artworkUrl60']
+    return render_to_response('query/listing_detail.html',
+                              {"json_objects": js_object["resultCount"],
+                               "icon": icon},
+                              context_instance=RequestContext(request))
