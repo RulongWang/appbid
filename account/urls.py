4b0d41edad22341ff0aad23e3a919e843918cef2
rulongwang <rulong.wang@gmail.com>
2013-09-05 22:43:24 +0800

    account setting

--- account/urls.py
+++ account/urls.py
@@ -1,12 +1,13 @@
 __author__ = 'rulongwang'
 from django.conf.urls import patterns, include, url
 from account.views import auth_home, register
-from account.views import login_view,logout_view, register
+from account.views import login_view,logout_view, register,account_settting
 from query.views import getDetail
 urlpatterns = patterns('',
     (r'^login/$', login_view),
     (r'^logout/$', logout_view),
     (r'^register/$', register),
     (r'^home/$',auth_home),
+    (r'^setting/$',account_settting),
 
 )
