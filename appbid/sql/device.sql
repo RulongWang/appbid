a4dbd781be54f934c2caa8d12205d6665af328d7
rulongwang <rulong.wang@gmail.com>
2013-09-23 20:46:41 +0800

    update homepage

--- appbid/sql/device.sql
+++ appbid/sql/device.sql
@@ -1,4 +1,3 @@
 insert into appbid_device(`device`) values('iPad');
 insert into appbid_device(`device`) values('iPhone');
 insert into appbid_device(`device`) values('iTouch');
-insert into appbid_device(`device`) values('iWatch');
