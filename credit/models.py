77ffa1b9005a9d877b5cede4c43312caeb332712
rulongwang <rulong.wang@gmail.com>
2013-10-03 21:42:19 +0800

    create new packages

---
+++ credit/models.py
@@ -0,0 +1,12 @@
+__author__ = 'rulongwang'
+
+from django.db import models
+from django.contrib.auth.models import User
+from django.contrib import admin
+
+
+class CreditCoin(models.Model):
+
+    user = models.OneToOneField(User)
+    credit_points = models.IntegerField(default=100) # credit point
+    credit_level = models.IntegerField(default=1) # User credit level
