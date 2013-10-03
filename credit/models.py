bb6cf6fd89c3a403081f51ec76773782f0fbdc24
rulongwang <rulong.wang@gmail.com>
2013-10-03 21:55:53 +0800

    update credit point

--- credit/models.py
+++ credit/models.py
@@ -4,11 +4,12 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.contrib import admin
 
+
 #credit change log + increase _reduce
 class CreditPoint(models.Model):
 
     user = models.OneToOneField(User)
-    credit_points = models.IntegerField(default=100)  # credit point
+    points = models.IntegerField(default=100)  # credit point
 
 
 class CreditLog(models.Model):
@@ -16,3 +17,5 @@ class CreditLog(models.Model):
     user = models.ForeignKey(User)
     credit_point = models.ForeignKey(CreditPoint)
     change_reason = models.TextField(null=True,blank=True)
+    points = models.IntegerField()
+
