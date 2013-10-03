0701e35895dc821f71ab42f4637b6c4a82b4723a
rulongwang <rulong.wang@gmail.com>
2013-10-03 21:50:52 +0800

    update credit package

--- credit/models.py
+++ credit/models.py
@@ -4,8 +4,15 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.contrib import admin
 
-
-class CreditCoin(models.Model):
+#credit change log + increase _reduce
+class CreditPoint(models.Model):
 
     user = models.OneToOneField(User)
     credit_points = models.IntegerField(default=100)  # credit point
+
+
+class CreditLog(models.Model):
+
+    user = models.ForeignKey(User)
+    credit_point = models.ForeignKey(CreditPoint)
+    change_reason = models.TextField(null=True,blank=True)
