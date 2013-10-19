a5d5b78c0ebaccc93ad275b865330d2977fab7f3
rulongwang <rulong.wang@gmail.com>
2013-10-19 23:32:41 +0800

    update email subscription

--- appbid/sql/subscriptionitem.sql
+++ appbid/sql/subscriptionitem.sql
@@ -1,12 +1,11 @@
 INSERT INTO `usersetting_subscriptionitem` (`id`,`item`) VALUES
- (1,'Notify me about changes to XXX itself'),
- (2,'Subscribe me to the XXX newsletter'),
- (3,'Allow XXX to send me special offers'),
- (4,'Notify me when I\'m sent a private message'),
- (5,'Notify me when a bid or an offer is placed on one of my listings'),
- (6,'Notify me when a comment is placed on one of my listings'),
- (7,'Notify me when a comment is placed on a listing I\'m watching'),
- (8,'Notify me when a bid is placed above one of my bids'),
- (9,'NoNotify me when new auctions appear for my watched categories'),
- (10,'Notify me when a seller I\'m watching creates new listings');
+ (1,'Subscribe newsletter'),
+ (2,'Allow Appswalk to send me special offers\/discount'),
+ (3,'Notify me when I\'m sent a private message'),
+ (4,'Notify me when a bid is placed on one of my Apps'),
+ (5,'Notify me when a comment is placed on one of my Apps'),
+ (6,'Notify me when a comment is placed on an App I\'m watching'),
+ (7,'Notify me when a bid is placed above one of my bids'),
+ (8,'Notify me when new auctions appear for my watched categories'),
+ (9,'Notify me when a seller I\'m watching creates new Apps');
 
