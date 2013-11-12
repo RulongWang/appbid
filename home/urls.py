73735654efbabae329d786792881155145dad87b
jarvisjia <kunpengzai@gmail.com>
2013-11-13 00:25:44 +0800

    Update the code of home page to add screw function.

--- home/urls.py
+++ home/urls.py
@@ -6,4 +6,5 @@ from home import views
 
 urlpatterns = patterns('',
     url(r'^$', views.home, name='home'),
+    url(r'^screw-home$', views.screwHome, name='screw_home'),
 )
--- home/views.py
+++ home/views.py
@@ -1,16 +1,32 @@
 __author__ = 'Jarvis'
 
+from django.views.decorators.csrf import csrf_protect
 from django.shortcuts import render_to_response, RequestContext
 from appbid import models as appModels
-from query.views import queryAppsWithPaginator
+from query import views as queryViews
 
 
+@csrf_protect
 def home(request, *args, **kwargs):
     """Query the apps info in home page."""
     initParam = {}
     page = request.GET.get('page', 1)
+    page_range = 2
     apps = appModels.App.objects.filter(status=2)
 
-    initParam['apps'] = queryAppsWithPaginator(request, page=page, apps=apps)
+    initParam['apps'] = queryViews.queryAppsWithPaginator(request, page_range=page_range, page=page, apps=apps)
 
     return render_to_response('home/home.html', initParam, context_instance=RequestContext(request))
+
+
+@csrf_protect
+def screwHome(request, *args, **kwargs):
+    """Query the apps info in screw home page."""
+    initParam = {}
+    page = request.GET.get('page', 1)
+    page_range = 2
+    apps = appModels.App.objects.filter(status=2)
+
+    initParam['apps'] = queryViews.queryAppsWithPaginator(request, page_range=page_range, page=page, apps=apps)
+
+    return render_to_response('home/screw.html', initParam, context_instance=RequestContext(request))
