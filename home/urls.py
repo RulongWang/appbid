262d0454e95b2f7c0e1fdbd87cfaefdb3033c943
Ruloong <me@rulong.org>
2013-11-14 15:28:59 +0800

    update footer links

--- home/urls.py
+++ home/urls.py
@@ -2,9 +2,11 @@ __author__ = 'rulongwang'
 
 from django.conf.urls import patterns, url
 from home import views
+from help import views as helpView
 
 
 urlpatterns = patterns('',
     url(r'^$', views.home, name='home'),
+    url(r'^termsandconditions$',helpView.terms,name='terms'),
     url(r'^screw-home$', views.screwHome, name='screw_home'),
 )
