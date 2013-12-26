b18a3fc5c1dc9853f4acae4dd0b5ab6decfba486
jarvisjia <kunpengzai@gmail.com>
2013-12-26 18:00:30 +0800

    Update the code of job.

--- job/job.py
+++ job/job.py
@@ -1,26 +1,26 @@
 __author__ = 'Jarvis'
 
-import datetime
-import jobdetail
+# import datetime
+# import jobdetail
 
 
-def jobByFourHour(*args, **kwargs):
-    """The task will be done every four hours."""
-    jobdetail.verificationAppForSeller()
+# def jobByFourHour(*args, **kwargs):
+#     """The task will be done every four hours."""
+#     jobdetail.verificationAppForSeller()
 
 
 def jobByMidnight(*args, **kwargs):
     """Server invoke the method to do schedule task every night at midnight."""
-    print 'jobForMidnight', datetime.datetime.now()
+    print 'jobForMidnight'
 
 
-def jobByEveryHour(*args, **kwargs):
-    """Server invoke the method to do schedule task at schedule time, such as: every hour."""
-    jobdetail.checkServiceDateForApps()
-    jobdetail.taskForBuyUnpaid()
-    jobdetail.checkIfSellApp()
+# def jobByEveryHour(*args, **kwargs):
+#     """Server invoke the method to do schedule task at schedule time, such as: every hour."""
+#     jobdetail.checkServiceDateForApps()
+#     jobdetail.taskForBuyUnpaid()
+#     jobdetail.checkIfSellApp()
 
 
-def jobByFiveMin(*args, **kwargs):
-    """Server invoke the method to do schedule task at schedule time, such as: every five min."""
-    jobdetail.jobPayStatus()
+# def jobByFiveMin(*args, **kwargs):
+#     """Server invoke the method to do schedule task at schedule time, such as: every five min."""
+#     jobdetail.jobPayStatus()
