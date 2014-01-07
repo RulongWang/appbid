a14b25e440395a48503050a0f3bd10b724c35c3b
Rulong <me@rulong.org>
2014-01-07 23:37:36 +0800

    update email setting

--- paypal/settings.py
+++ paypal/settings.py
@@ -2,9 +2,9 @@ __author__ = 'Jarvis'
 
 #paypal  setting
 #current setting only applys for production system
-PAYPAL_USER = 'rulong.wang_api1.yahoo.com'
-PAYPAL_PASSWORD = 'TZAW5SQZXSX7TSJN'
-PAYPAL_SIGNATURE = 'A5b2pgSRLLZVHA9BXjTbgs5Y3aMGA3bJuc8lIXdsSwOUs9F8rvw4wl7y'
+PAYPAL_USER = 'rulong.wang_api1.gmail.com'
+PAYPAL_PASSWORD = 'URJNN9Y396KJ6A6L'
+PAYPAL_SIGNATURE = 'AFcWxV21C7fd0v3bYYYRCpSSRl31A5YS8KempsL7BOPGQ6bUmzLqFq1A'
 PAYPAL_AP_APPLICATION_ID = 'APP-5RW64647DB339963H'
 PAYPAL_DEBUG = False # for sandbox. False for real environment.
 
