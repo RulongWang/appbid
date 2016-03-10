a26943550f32e73d8cdeffee874e11c977d77cd7
Rulong <rulong.wang@gmail.com>
2016-03-10 15:23:29 +0800

    Update settings.py

--- paypal/settings.py
+++ paypal/settings.py
@@ -2,13 +2,13 @@ __author__ = 'Jarvis'
 
 #paypal  setting
 #current setting only applys for production system
-PAYPAL_USER = 'rulong.wang_api1.gmail.com'
-PAYPAL_PASSWORD = 'URJNN9Y396KJ6A6L'
-PAYPAL_SIGNATURE = 'AFcWxV21C7fd0v3bYYYRCpSSRl31A5YS8KempsL7BOPGQ6bUmzLqFq1A'
-PAYPAL_AP_APPLICATION_ID = 'APP-5RW64647DB339963H'
+PAYPAL_USER = '...'
+PAYPAL_PASSWORD = ''
+PAYPAL_SIGNATURE = ''
+PAYPAL_AP_APPLICATION_ID = 'APP-'
 PAYPAL_DEBUG = False # for sandbox. False for real environment.
 
 #AppsWalk paypal account
-APPSWALK_ACCOUNT = 'rulong.wang@gmail.com'
+APPSWALK_ACCOUNT = '.@.com'
 
 
